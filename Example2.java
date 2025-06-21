cdafee8128eeffe8865a6af3d6f0c6063d7bdb08
MOORTHIRAJ R <132340874+MOORTHIRAJ-R@users.noreply.github.com>
2025-06-21 19:16:40 +0530

    Add files via upload

---
+++ Example2.java
@@ -0,0 +1,26 @@
+package test;
+import java.util.Scanner;
+public class Example2
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number to print pattern");
+        int n  = scanner.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int spaces = n - (i + 1); // n = 4 i = 3, 4 -(3 + 1) = 4 - 4 = 0 ;
+            for(int k = 1; k<= spaces;k++)
+                System.out.print(" ");
+            //left side
+            for(int j = i;j >=0;j--)
+                System.out.print(j);
+            //Right Side
+            for(int j = 1;j <= i;j++)
+                System.out.print(j);
+            if(i != n-1)
+                System.out.println();
+        }
+    }
+}
+	
